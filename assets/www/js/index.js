3e43282d60d49c545b1f34a5c3821a028d02f0c5
Arvind <arvin09@gmail.com>
2013-11-08 11:37:40 +0530

    Implemented  the  following things 1. Splash Screen 2. Home Screen 3. Dummy Interaction

--- assets/www/js/index.js
+++ assets/www/js/index.js
@@ -1,49 +1,74 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-var app = {
-    // Application Constructor
-    initialize: function() {
-        this.bindEvents();
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicity call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-        app.receivedEvent('deviceready');
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
+var displayPic = function(){}
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
+displayPic.prototype = {
 
-        console.log('Received Event: ' + id);
-    }
-};
+		busyTime: 1000,
+		splashTime:3000,
+
+
+		initialize: function() {
+			console.log("Application Initialized");
+			this.bindEvents();
+			this.initPageEvents();
+		},
+
+		bindEvents: function() {
+			var self = this;
+			console.log("Bind Events");
+			document.addEventListener("deviceready", this.onDeviceReady, false);
+		},
+
+		onDeviceReady: function(){
+			console.log("Device is Ready");
+		},
+
+		initPageEvents: function(){
+			var self = this;
+			console.log("init Page Events");
+			window.setTimeout(function () {
+				self.initHome.call(this,self);
+			}, this.splashTime);
+		},
+
+		initHome : function(self,e){
+			if (!self.login) {
+				$.mobile.changePage("#home", { transition: "flip" });
+			}
+		},
+
+		fauxAjax : function fauxAjax(func, text, thisObj) {
+			$.mobile.loading('show', { theme: 'a', textVisible: true, text:text });
+			window.setTimeout(function () {
+				$.mobile.loading('hide');
+				func();
+
+			}, this.busyTime);
+		},
+
+
+		/*handleLogOn : function (ff, success) {
+		var self = this;
+		if (success) {
+			_ffNum = ff;
+			$.mobile.changePage('#home', { transition: 'slide' });
+			//self.getDataforFF(_ffNum,self.handleDataForFF);
+		}
+	},*/
+
+		/*getDataforFF : function(id, callback) {
+		var self = this;
+		self.fauxAjax(function () {
+			callback(ffInfo);
+		}, 'getting your data ...', self);
+	},*/
+
+		/*handleDataForFF : function () {
+		 $.mobile.changePage('#home', { transition: 'flip' });
+	 }*/
+
+
+}
+
+
+
+
